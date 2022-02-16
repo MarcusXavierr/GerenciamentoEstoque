d76bbc263e267ec9bc7d824fc7d553b9a32085a9
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-16 16:59:01 -0300

    create migrations

---
+++ database/migrations/2022_02_16_190945_create_stocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id', 70);
+            $table->integer('products_in_stock');
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('SKU')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
