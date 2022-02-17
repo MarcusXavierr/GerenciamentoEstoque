b3ea68e23674a0eeac136bc711dc0ad727ec5ad4
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-16 23:27:57 -0300

    publish notify

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    @notifyCss
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
@@ -80,10 +81,11 @@
         </nav>
 
         <main class="py-4">
+            <x:notify-messages />
             @yield('content')
         </main>
     </div>
-
+    @notifyJs
     @yield('scripts')
 </body>
 </html>
