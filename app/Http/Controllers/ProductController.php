e45677bb8badbb733bec0fd28235dbafb13ab8e8
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-17 14:17:47 -0300

    Add auth logic

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,6 +17,7 @@ class ProductController extends Controller
     public function __construct()
     {
         $this->treatString = new TreatStringInput();
+        $this->middleware('auth');
     }
 
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,9 +38,11 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+                        @auth
                         <li class="nav-item">
                             <a href="{{route('product.index')}}" class="nav-link">Produtos</a>
                         </li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
