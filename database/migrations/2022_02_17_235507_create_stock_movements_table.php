e109ab705ceaa8dad1a7d45799b432d0233764d6
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-17 21:14:37 -0300

    create stock movements table

---
+++ database/migrations/2022_02_17_235507_create_stock_movements_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockMovementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_movements', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id', 70);
+            $table->unsignedBigInteger('stock_id');
+            $table->boolean('is_api');
+            $table->Integer('products_movemented');
+            $table->foreign('product_id')->references('SKU')->on('products');
+            $table->foreign('stock_id')->references('id')->on('stocks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_movements');
+    }
+}
