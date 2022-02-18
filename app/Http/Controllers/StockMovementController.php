a56bba1147db6120be8a3ff20108994e8c423cc1
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-17 21:14:53 -0300

    Create api endpoints and controller

---
+++ app/Http/Controllers/StockMovementController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StockMovementController extends Controller
+{
+
+
+    public function index()
+    {
+    }
+
+
+    public function screenAddStock()
+    {
+    }
+
+
+    public function screenRemoveStock()
+    {
+    }
+
+    public function addStock(Request $request)
+    {
+        dd($request);
+    }
+
+    public function removeStock(Request $request)
+    {
+        dd($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StockMovementController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/api/adicionar-produtos', "App\Http\Controllers\StockMovementController@addStock")->name('stock.add');
+Route::post('/api/baixar-produtos', "App\Http\Controllers\StockMovementController@removeStock")->name('stock.remove');
