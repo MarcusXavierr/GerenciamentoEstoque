47c1703583119377dd00e9dd044e6270b148a21d
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-19 17:28:21 -0300

    change home route

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('product', 'App\Http\Controllers\ProductController');
 
 Route::get('/adicionar-produtos/{id}', [StockMovementMvcController::class, 'showAddProductPage'])->name('stock.add.show');
