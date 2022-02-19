c2bb5dc6bcea5ed3cc73b9fe045ad658ab19682c
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-19 15:25:36 -0300

    create a page to list stock

---
+++ resources/views/stock_movements/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h2>Listagem de produtos</h2>
+        <a class="btn btn-success" href="{{route('product.create')}}">Criar produto</a>
+        <table class="table table-bordered table-responsive mt-3">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Estoque Atual</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $product)
+                    <tr>
+                        <td>{{$product->name}}</td>
+                        
+                        <td>{{$product->stock->products_in_stock}}</td>
+                        <td>
+                            <div class="btn-group flex-wrap">
+                                <a 
+                                href="{{route('stock.add.show', 
+                                ['id' => $product->stock->id])}}"
+                                class="btn btn-outline-success">Adicionar estoque</a>
+
+                                <a 
+                                href="{{route('stock.remove.show', 
+                                ['id' => $product->stock->id])}}"
+                                class="btn btn-outline-danger">Dar baixa em produtos</a>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{$products->links()}}
+    </div>
+@endsection
