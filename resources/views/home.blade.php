3ce6fe99cbf3fecb2672fc3c91c04c3f6c2e83f7
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-19 17:28:35 -0300

    add relatory info in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,44 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <h3>Seja bem vindo(a) de volta ao sistema de gerenciamento de estoque</h3>
+    <p>Veja abaixo o relatório de hoje sobre de produtos adicionados no estoque e de baixas nos produtos</p>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Produto</th>
+                <th class="col-1">Quantidade</th>
+                <th class="col-2">Sistema usado</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($relatory as $stock)
+                <tr>
+                    <td>{{$stock->name}}</td>
+                    <td>
+                        @if($stock->products_movemented > 0)
+                            <span style="color:green">
+                                <i class="fal fa-caret-up"></i>
+                                {{$stock->products_movemented}}
+                            </span>
+                        @else
+                            <span style="color:red">
+                                <i class="fal fa-caret-down"></i>
+                                {{$stock->products_movemented}}
+                            </span>
+                        @endif
+                    </td>
+                    <td>
+                        @if($stock->is_api)
+                            API
+                        @else
+                            Sistema interno
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 @endsection
