9b28b1b384466f72690bf8f054190163824bffd4
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-19 00:13:30 -0300

    create addProduct in stock method

--- app/Http/Controllers/StockMovementMvcController.php
+++ app/Http/Controllers/StockMovementMvcController.php
@@ -2,14 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\SaveStockMovementReport;
 use App\Models\Product;
+use App\Models\Stock;
 use Illuminate\Http\Request;
 
 class StockMovementMvcController extends Controller
 {
+    private SaveStockMovementReport $saveReport;
+
+    public function __construct()
+    {
+        $this->saveReport = new SaveStockMovementReport();
+    }
+
     public function showAddProductPage($id)
     {
         $product = Product::findOrFail($id);
         return view('stock_movements.add_stock', compact('product'));
+    }
+
+    public function addProduct(Request $request, $id)
+    {
+        $data = $request->all();
+        $stock = Stock::findOrFail($id);
+
+        $isUpdated = $stock->update([
+            'products_in_stock' => $data['quantity'] + $stock->products_in_stock
+        ]);
+
+        if (!$isUpdated) {
+            return 'Error';
+        }
+        $this->saveReport->saveReportOfMovementOnDB($stock->product_id, $stock->id, $data['quantity'], false);
+        return 'Success : ' . $stock->products_in_stock;
     }
 }
