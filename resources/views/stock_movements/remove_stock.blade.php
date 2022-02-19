8502d7cf295ef8af791ffec18412a53d7a7034e4
MarcusXavierr <marcusxavierr123@gmail.com>
2022-02-19 00:12:50 -0300

    create basic page for removeProduct method

---
+++ resources/views/stock_movements/remove_stock.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+    <div class="container">
+        <h2>Dar baixa no estoque do produto</h2> 
+       <div class="text-center">
+            <h4 class="mt-3">{{$product->name}}</h4>
+            <p>{{$product->description}}</p>
+            <p>Preço: R${{number_format($product->price, 2, ',', '.')}}</p>
+            <p>Estoque atual: {{$product->stock->products_in_stock}}</p>
+       </div>
+        <form action="{{route('stock.remove', ['id' => $product->stock->id])}}" method="post">
+            @csrf
+            <div class="input-group">
+                <input type="number" name="quantity" class="form-control">
+                <button class="btn btn-outline-primary" >Enviar</button>
+            </div>
+            
+        </form>
+    </div>
+@endsection
